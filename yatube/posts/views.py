5e859d2c5d29e610ff6777cbc7ebb610a011343d
Василий Ковач <vasiliy.kovac@bk.ru>
2022-11-10 21:59:18 +0300

    Правки после ревью

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -116,12 +116,10 @@ def follow_index(request):
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
     if author != request.user:
-        if Follow.objects.filter(user=request.user, author=author).exists():
-            return redirect('posts:follow_index')
-    Follow.objects.create(
-        user=request.user,
-        author=author
-    )
+        Follow.objects.create(
+            user=request.user,
+            author=author
+        )
     return redirect('posts:follow_index')
 
 
